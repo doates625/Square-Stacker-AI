5195dc10e01e4d850c4d6d02e8d44d71d81ebc54
Dan <doates625@gmail.com>
2019-09-23 19:06:27 -0400

    Added basic class for representing game state as data

---
+++ Test.py
@@ -0,0 +1,14 @@
+'''
+Test.py
+Square Stacker game test script
+
+'''
+from GameState import GameState
+
+# Create empty game
+state = GameState()
+
+# Display game
+for i in range(2):
+    print('Game State:')
+    print(state)
